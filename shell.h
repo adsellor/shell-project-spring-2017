6155725c3abfef41deded791fc00cbcde6d3a63f
Gasia Atashian <gasiaatashian@gmail.com>
2017-03-11 01:40:28 +0400

    fork exec

--- shell.h
+++ shell.h
@@ -1,8 +1,11 @@
 #pragma once
 #include <iostream>
+#include <unistd.h>
 #include <string>
 #include <vector>
 #include <sstream>
+#include <sys/types.h>
+#include <sys/wait.h>
 
 void get_comand(void);
 std::vector <std::string> parasing (std::string inputLine);
